48b60ca7221bea95b120bce3652d730ca7bec16f
PrajwalMendon <64822182+PrajwalMendon@users.noreply.github.com>
2020-05-06 19:18:51 -0700

    Update diceroller.py

--- diceroller.py
+++ diceroller.py
@@ -1,11 +1,17 @@
 import random
 
 def diceroller():
-    rollagain = 'yes'
-    while rollagain == 'yes' or 'y':
-        sides = int(input('Select a number '))
+    rollagain = 'y'
+    rolltotal = 0
+    rollcount = 0
+    while rollagain == 'y':
+        sides = int(input('Choose the maximum sides of the dice: '))
         result = random.randrange(1, sides)
+        rolltotal = result + rolltotal
+        rollcount = 1 + rollcount
         print(result)
-        rollagain = input('Roll Again? ').lower()
-
+        rollagain = input('Roll again (Y/N)? ').lower()
+        if rollagain != 'y':
+            print('The sum of all rolls is: ' + str(rolltotal))
+            print('You have rolled ' + str(rollcount) + ' times.')
 diceroller()
